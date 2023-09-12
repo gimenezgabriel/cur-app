582e79a468b9407543edae6e7a58de6f732cc4a7
gimenezgabriel <gimenezgabriel705@gmail.com>
2023-09-12 01:08:59 -0300

    primer commmit

---
+++ src/components/Card/Card.style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../constants/Colors";
+
+export default styles = StyleSheet.create({
+    container:{
+        backgroundColor: colors.secondary ,
+        borderRadius: 8,
+        shadowColor: '#06292e',
+        shadowOffset: {height: 5, width: 3},
+        elevation: 10,
+        shadowOpacity: 1,
+        shadowRadius: 8,
+        },
+    })
---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../constants/Colors";
+
+
+export default styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.Primary ,
+        height: 60,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 25,
+        color: '#ffffff',
+    },
+})
