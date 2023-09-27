1cfd9bd061e82400983aeb6315e878e039477762
gimenezgabriel <gimenezgabriel705@gmail.com>
2023-09-27 18:22:36 -0300

    desafio tabnavigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,26 @@
-import { Text, View, TouchableOpacity, Image } from 'react-native'
+import { Text, View, TouchableOpacity, Image } from "react-native";
 
-import React from 'react'
-import styles from './Header.style'
+import React from "react";
+import styles from "./Header.style";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Header = ({ name, title, navigation }) => {
   return (
-    <View style={styles.container}> 
-    {name !== 'Home' &&
-      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
-          <Text style={styles.bakButtonText}
-          >Back</Text>
+    <View style={styles.container}>
+      {name !== "Home" && (
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.backButton}
+        >
+
+          <Ionicons name="chevron-back-outline" size={30} color="#ffffff" />
         </TouchableOpacity>
-}
+      )}
       <View style={styles.headerContainer}>
-      <Text style={styles.text}>{title}</Text>
-      {/* <Image
-          source={require('../../../assets/images/logocur.png')} // Reemplaza con la ruta de tu imagen PNG
-          style={styles.logo} // Define un estilo para tu logo si es necesario
-        /> */}
+        <Text style={styles.text}>{title}</Text>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
